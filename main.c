713808618cd25bec935521e5fb2666a64fb322b6
kyle <kylesch115@gmail.com>
2024-04-02 22:28:22 -0600

    center square

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <math.h>
+#include <sys/ioctl.h>
 
 #define HIDE_CURSOR() printf("\e[?25l")
 #define SHOW_CURSOR() printf("\e[?25h")
@@ -10,7 +11,8 @@
 int border_width = 80;
 int border_height = 45;
 
-void rotate(double theta, double *x, double *y) {
+void rotate(double theta, double *x, double *y)
+{
   double theta_rad = theta * 3.14 / 180;
 
   double new_x = (*x * cos(theta_rad)) - (*y * sin(theta_rad));
@@ -18,26 +20,24 @@ void rotate(double theta, double *x, double *y) {
 
   *x = new_x;
   *y = new_y;
-
-  // printf("%f %f\n", new_x, new_y);
 };
 
-void draw(int n, double xpos[], double ypos[])
+void draw(int n, unsigned short wheight, unsigned int wwidth, double xpos[], double ypos[])
 {
-  for (int i = 0; i < border_height; i++)
+  for (int i = 0; i < wheight - 1; i++)
   {
-    for (int j = 0; j < border_width; j++)
+    for (int j = 0; j < wwidth - 1; j++)
     {
       int do_draw = 0;
       for (int k = 0; k < n; k++)
       {
-        double x = round(xpos[k] + 50);
-        double y = round(ypos[k] + 20);
+        double x = round(xpos[k] + (wwidth / 2));
+        double y = round(ypos[k] + (wheight / 2));
         if ((abs((double)i - y) < 1) && (abs((double)j - x) < 1))
         {
           do_draw = 1;
           printf("*");
-          // break;
+          break; // need this?
         }
       }
 
@@ -52,6 +52,14 @@ void draw(int n, double xpos[], double ypos[])
 
 int main()
 {
+  struct winsize ws;
+
+  if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1)
+  {
+    perror("ioctl");
+    return 1;
+  }
+
   HIDE_CURSOR();
 
   // square
@@ -59,25 +67,27 @@ int main()
   int square_height = 20;
   int square_width = 20;
 
-  // int n = 56;
   int n = (2 * square_height) + (2 * square_width) - 4;
   double xpos[n];
   double ypos[n];
-  
+
+  int i_range = square_height / 2;
+  int j_range = square_width / 2;
+
   int idx = 0;
-  for (int i = 0; i < square_height; i++)
+  for (int i = -i_range; i < i_range; i++)
   {
-    for (int j = 0; j < square_width; j++)
+    for (int j = -j_range; j < j_range; j++)
     {
       // top/bottom
-      if (i == 0 || i == square_height - 1)
+      if (i == -i_range || i == i_range - 1)
       {
         xpos[idx] = j;
         ypos[idx] = i;
         idx++;
       }
       // left/right
-      else if (j == 0 || j == square_width - 1)
+      else if (j == -j_range || j == j_range - 1)
       {
         xpos[idx] = j;
         ypos[idx] = i;
@@ -89,13 +99,14 @@ int main()
 
   while (1)
   {
-    // MOVE_CURSOR();
+    MOVE_CURSOR();
     system("clear");
-    draw(n, xpos, ypos);
-    
+    draw(n, ws.ws_row, ws.ws_col, xpos, ypos);
+
     for (int k = 0; k < n; k++)
     {
-      rotate(10, &xpos[k], &ypos[k]);
+      int r = 10;
+      rotate(r, &xpos[k], &ypos[k]);
     }
 
     double wait_s = 0.05;
