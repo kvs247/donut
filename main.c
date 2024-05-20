b0b332f05d05e0998788084f3c320d43f1c4ce9e
Kyle <kylesch115@gmail.com>
2024-05-19 19:07:02 -0600

    draw square corners and fill in (needs polish/has bugs)

--- main.c
+++ main.c
@@ -82,32 +82,90 @@ void draw2(int n, unsigned short wheight, unsigned int wwidth, double xpos[], do
   get_arr_min_max(&xmin, &xmax, n, xpos);
   get_arr_min_max(&ymin, &ymax, n, ypos);
 
-  // printf("%f_%f_%f_%f", xmin, xmax,ymin, ymax);
+  double point0_x = xpos[0];
+  double point0_y = ypos[0];
+
+  double point1_x = xpos[1];
+  double point1_y = ypos[1];
+
+  double point2_x = xpos[2];
+  double point2_y = ypos[2];
+
+  double point3_x = xpos[3];
+  double point3_y = ypos[3];
+
+  double slope01 = (point1_y - point0_y) / (point1_x - point0_x);
+  double slope12 = (point2_y - point1_y) / (point2_x - point1_x);
+  double slope23 = (point3_y - point2_y) / (point3_x - point2_x);
+  double slope30 = (point0_y - point3_y) / (point0_x - point3_x);
+
+  int is_side_01_top = 0;
+  if (point0_y == ymax || point1_y == ymax)
+  {
+    is_side_01_top = 1;
+  }
+
+  int is_side_12_top = 0;
+  if (point1_y == ymax || point3_y == ymax)
+  {
+    is_side_12_top = 1;
+  }
 
   for (int i = 0; i < wheight - 1; i++)
   {
     for (int j = 0; j < wwidth - 1; j++)
     {
       int do_draw = 0;
-      for (int k = 0; k < n; k++)
+
+      double xoffset = wwidth / 2;
+      double yoffset = wheight / 2;
+
+      double x = j;
+      double y = i;
+
+      double side_01 = (slope01 * (x - (point0_x + xoffset))) + (point0_y + yoffset);
+      double side_23 = (slope23 * (x - (point2_x + xoffset))) + (point2_y + yoffset);
+      
+      double side_12 = (slope12 * (x - (point1_x + xoffset))) + (point1_y + yoffset);
+      double side_30 = (slope30 * (x - (point3_x + xoffset))) + (point3_y + yoffset);
+
+      int condition = 0;
+      if (is_side_01_top && is_side_12_top)
       {
-        double x = round(xpos[k] + (wwidth / 2));
-        double y = round(ypos[k] + (wheight / 2));
-        if ((abs((double)i - y) < 1) && (abs((double)j - x) < 1))
-        {
-          do_draw = 1;
-          printf("*");
-          break; // need this?
-        }
+        condition = y <= side_01 && y >= side_23 && y <= side_12 && y >= side_30;
+      }
+      else if (is_side_01_top && !is_side_12_top)
+      {
+        condition = y <= side_01 && y >= side_23 && y >= side_12 && y <= side_30;
+      }
+      else if (!is_side_01_top && is_side_12_top)
+      {
+        condition = y >= side_01 && y <= side_23 && y <= side_12 && y >= side_30;
+      }
+      else
+      {
+        condition = y >= side_01 && y <= side_23 && y >= side_12 && y <= side_30;
       }
 
-      if (!do_draw)
+      if (condition)
+      {
+        do_draw = 1;
+      }
+
+      if (do_draw)
+      {
+        printf("*");
+      }
+      else
       {
         printf(" ");
       }
     }
     printf("\n");
   }
+  printf(is_side_12_top ? "right\n" : "left\n");
+  printf(is_side_01_top ? "top\n" : "bot\n");
+  printf("\n%f (%f, %f) (%f, %f) (%f, %f) (%f, %f)\n", ymax, point0_x, point0_y, point1_x, point1_y, point2_x, point2_y, point3_x, point3_y);
 }
 
 void make_square_border(int *n, double **xpos, double **ypos)
@@ -185,8 +243,8 @@ void make_corners(int *n, double **xpos, double **ypos)
 
   (*ypos)[0] = 10.0;
   (*ypos)[1] = -10.0;
-  (*ypos)[2] = 10.0;
-  (*ypos)[3] = -10.0;
+  (*ypos)[2] = -10.0;
+  (*ypos)[3] = 10.0;
 }
 
 int main()
@@ -220,8 +278,8 @@ int main()
       rotate(r, &xpos[k], &ypos[k]);
     }
 
-    double wait_s = 0.05;
-    // double wait_s = 1;
+    // double wait_s = 0.05;
+    double wait_s = 0.2;
     usleep(wait_s * 1000000);
   }
 
